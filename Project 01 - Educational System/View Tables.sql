ec642d425c7fa3f71ce5f2b40abc80c8ab3d5866
scryng <scrynng@gmail.com>
2024-05-10 17:35:50 -0300

    Entrega Projeto 01 - Otimizado

--- Project 01 - Educational System/View Tables.sql
+++ Project 01 - Educational System/View Tables.sql
@@ -3,20 +3,28 @@
 -- ########################################################
 
 -- Visualiza a tabela de Estudantes
-SELECT * FROM Students
+    SELECT * FROM Students
+
 -- Visualiza a tabela de Telefones
-SELECT * FROM Telephones
+    SELECT * FROM Telephones
+
 -- Visualiza a tabela de Emails
-SELECT * FROM Emails
+    SELECT * FROM Emails
+
 -- Visualiza a tabela de Departamentos
-SELECT * FROM Departments
+    SELECT * FROM Departments
+
 -- Visualiza a tabela de Cursos
-SELECT * FROM Courses
+    SELECT * FROM Courses
+
 -- Visualiza a tabela de Matrículas
-SELECT * FROM Records
+    SELECT * FROM Records
+
 -- Visualiza a tabela de Disciplinas
-SELECT * FROM Disciplines
+    SELECT * FROM Disciplines
+
 -- Visualiza a tabela de Matricula_Disciplinas
-SELECT * FROM Reco_Disc
+    SELECT * FROM Reco_Disc
+
 -- Visualiza a tabela de Cursos_Disciplinas
-SELECT * FROM Cour_Disc
+    SELECT * FROM Cour_Disc
